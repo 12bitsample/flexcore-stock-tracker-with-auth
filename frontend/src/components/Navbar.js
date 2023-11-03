e2bd4ad8d62bac87d7b3160affe3f9aaee3c3ea3
Michael McAdow <michaelmcadow@gmail.com>
2023-11-03 06:33:35 -0400

    11323

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
     
     const handleClick = () => {
         logout();
@@ -15,13 +17,19 @@ const Navbar = () => {
                     <h1>Flex-Core Raw Core Tracker</h1>
                 </Link>
                 <nav>
-                    <div id="div-logout">
-                        <button onClick={handleClick}>Logout</button>
-                    </div>
-                    <div className="link-div">
+                    {/* conditionally render info dependent on login status */}
+                    {user && (
+                        <div id="div-logged-in">
+                            <span>{user.username}</span>
+                            <button onClick={handleClick}>Logout</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div id="div-logged-out">
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Sign Up</Link>
                     </div>
+                    )}
                 </nav>
             </div>
         </header>
